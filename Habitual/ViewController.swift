b69d83ac41352ebb8ae029210d00fa174f0eb579
Rushani Shawuti <ruhsane924@gmail.com>
2018-12-09 19:38:15 -0800

    initial commit

---
+++ Habitual/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Habitual
+//
+//  Created by Ruhsane Sawut on 12/9/18.
+//  Copyright © 2018 ruhsane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
